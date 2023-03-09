376449c51d053785895015b8f50f74b68a0c9dd3
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2023-03-09 14:42:31 -0300

    fix: test message

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -1,7 +1,7 @@
-import { AuthenticatedRequest } from "@/middlewares";
-import enrollmentsService from "@/services/enrollments-service";
-import { Response } from "express";
-import httpStatus from "http-status";
+import { AuthenticatedRequest } from '@/middlewares';
+import enrollmentsService from '@/services/enrollments-service';
+import { Response } from 'express';
+import httpStatus from 'http-status';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -29,15 +29,14 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 }
 
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
-  const { cep } = req.query as Record<string, string>;
+  // const { cep } = req.query as Record<string, string>;
 
   try {
-    const address = await enrollmentsService.getAddressFromCEP(cep);
+    const address = await enrollmentsService.getAddressFromCEP();
     res.status(httpStatus.OK).send(address);
   } catch (error) {
-    if (error.name === "NotFoundError") {
+    if (error.name === 'NotFoundError') {
       return res.send(httpStatus.NO_CONTENT);
     }
   }
 }
-
